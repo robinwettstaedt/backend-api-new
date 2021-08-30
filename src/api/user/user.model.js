e14f4035b0b851a5e917cc9a69f25c340be26d59
robinwettstaedt <robinwettstadt@gmail.com>
2021-08-30 22:48:08 +0200

    schema additions

--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  picture: {
+    type: String,
+    default: 'defaultpicture.com',
+  },
   settings: {
     theme: {
       type: String,
